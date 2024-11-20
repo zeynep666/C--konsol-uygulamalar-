4daeaf3633bddd08f6acf683ae71003c08e1fc9c
zeynep666 <zeynepecevit666@gmail.com>
2024-11-20 23:30:17 +0300

    Add files via upload
    Girilen sayının armstrong sayısı olup olmadığına bakar.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Bir sayı giriniz: ");
+        int sayi = Convert.ToInt32(Console.ReadLine());
+
+        int Sayi = sayi;
+        int basamakSayisi = sayi.ToString().Length; 
+        int toplam = 0;
+
+        while (sayi > 0)
+        {
+            int basamak = sayi % 10; 
+            toplam += (int)Math.Pow(basamak, basamakSayisi); 
+            sayi /= 10; 
+        }
+
+        if (toplam == Sayi)
+        {
+            Console.WriteLine($"{Sayi} sayısı bir armstrong sayıdır.");
+        }
+        else
+        {
+            Console.WriteLine($"{Sayi} sayısı bir armstrong sayı değildir.");
+        }
+
+        Console.ReadLine();
+    }
+}
