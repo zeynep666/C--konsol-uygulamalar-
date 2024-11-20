b3f33daffacb58bb488150878e9eec68f4fce3e1
zeynep666 <zeynepecevit666@gmail.com>
2024-11-20 23:37:59 +0300

    Add files via upload
    Girilen sayıya kadar olan sayıların toplamını alır.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Bir sayı giriniz: ");
+        int sayi = Convert.ToInt32(Console.ReadLine());
+        int toplam = 0;
+
+        for (int i = 0; i < sayi; i++)
+        {
+            toplam += i;
+        }
+
+        Console.WriteLine($"{sayi} sayısına kadar olan sayıların toplamı: {toplam}");
+        Console.ReadLine();
+    }
+}
