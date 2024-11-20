75c64a8b3abee783e406ac1b0e0d156974cd2cd9
zeynep666 <zeynepecevit666@gmail.com>
2024-11-20 23:36:07 +0300

    Add files via upload
    Sayının mükemmel sayı olup olmadığına bakar.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Bir sayı giriniz: ");
+        int sayi = Convert.ToInt32(Console.ReadLine());
+
+        int toplam = 0;
+        //sayının mükemmel sayı olup olmadığını bulmak için sayının tam bölenlerini buluyoruz.
+        for (int i = 1; i < sayi; i++)
+        {
+            if (sayi % i == 0)
+            {
+                toplam += i;
+            }
+        }
+
+        // mükemmel sayı olup olmadığını kontrol ediyoruz.
+        if (toplam == sayi)
+        {
+            Console.WriteLine($"{sayi} bir mükemmel sayıdır.");
+        }
+        else
+        {
+            Console.WriteLine($"{sayi} bir mükemmel sayı değildir.");
+        }
+
+        Console.ReadLine();
+    }
+}
