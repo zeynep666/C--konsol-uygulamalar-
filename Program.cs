5e1c71ad64a56527cac6e40e28830adf5ec74da2
zeynep666 <zeynepecevit666@gmail.com>
2024-11-20 23:40:39 +0300

    Add files via upload
    Not ortalaması hesaplar.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Ortalama
+{
+    static void Main()
+    {
+        Console.WriteLine("1. ders notunuzu giriniz:");
+        int not1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("2. ders notunuzu giriniz:");
+        int not2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("3. ders notunuzu giriniz:");
+        int not3 = Convert.ToInt32(Console.ReadLine());
+
+        double ortalama = (not1 + not2 + not3) / 3.0;
+        Console.WriteLine();
+        Console.WriteLine("Ortalamanız: "+ ortalama);
+        if (ortalama > 50)
+        {
+            Console.WriteLine("Dersi geçtiniz.");
+
+        }
+        if (ortalama < 50)
+        {
+            Console.WriteLine("Dersi geçemediniz.");
+        }
+    }
+}
+    
+
