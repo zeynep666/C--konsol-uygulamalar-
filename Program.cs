1f4d19db8d3673c5e18cb9e612e5f1f4e1d5b553
zeynep666 <zeynepecevit666@gmail.com>
2024-11-20 23:45:52 +0300

    Add files via upload
    Girilen değerlerin üçgen oluşturup oluşturmadığına bakar.

--- Program.cs
+++ Program.cs
@@ -1,61 +1,36 @@
 ﻿using System;
 
-namespace siniflar
+class Program
 {
-    class Ogrenci
+    static void Main()
     {
-        public string[] Dersler { get; set; }
-        public int DersSayisi { get; set; }
-    }
+        // Kullanıcıdan üç değer alıyoruz
+        Console.Write("A değerini giriniz: ");
+        int a = Convert.ToInt32(Console.ReadLine());
 
-    class Ogretmen
-    {
-        public string[] VerdigiDersler { get; set; }
-        public int DersSayisi { get; set; }
-    }
+        Console.Write("B değerini giriniz: ");
+        int b = Convert.ToInt32(Console.ReadLine());
 
-    class Program
-    {
-        static void Ogrenciprogrami(Ogrenci ogr)
+        Console.Write("C değerini giriniz: ");
+        int c = Convert.ToInt32(Console.ReadLine());
+
+        // Aldığımız değerleri topluyoruz
+        int toplam = a + b + c;
+
+        // Değerler toplamının 180 olup olmadığını kontrol ediyoruz
+        //180e eşit ise üçgendir değil ise üçgen değildir yazdırıyoruz
+        if (toplam < 180)
         {
-            Console.WriteLine($"\nÖğrencinin ders programı ({ogr.DersSayisi} ders):");
-            foreach (var ders in ogr.Dersler)
-            {
-                Console.WriteLine($"- {ders}");
-            }
+            Console.WriteLine("Bu bir üçgen değildir");
         }
-
-        static void Ogretmenprogrami(Ogretmen ogrt)
+        else if (toplam > 180)
         {
-            Console.WriteLine($"\nÖğretmenin dersleri ({ogrt.DersSayisi} ders):");
-            foreach (var ders in ogrt.VerdigiDersler)
-            {
-                Console.WriteLine($"- {ders}");
-            }
+            Console.WriteLine("Bu bir üçgen değildir");
         }
-
-        static void Main(string[] args)
+        else
         {
-            Console.WriteLine("Hoş geldiniz! Lütfen bir seçim yapınız:");
-            Console.WriteLine("1. Öğretmen");
-            Console.WriteLine("2. Öğrenci");
-            Console.Write("Seçiminiz: ");
-            int secim = Convert.ToInt32(Console.ReadLine());
-            Ogrenci ogrenci = new Ogrenci { Dersler = new string[] { "Algoritma", "Yazılım" }, DersSayisi = 2 };
-            Ogretmen ogretmen = new Ogretmen { VerdigiDersler = new string[] { "Algoritma", "Yazılım" }, DersSayisi = 2 };
-            if (secim == 1)
-            {
-                Ogretmenprogrami(ogretmen);
-            }
-            else if (secim == 2)
-            {
-                Ogrenciprogrami(ogrenci);
-            }
-            else
-            {
-                Console.WriteLine("Geçersiz seçim!");
-            }
-            Console.ReadKey();
+            Console.WriteLine("Bu bir üçgendir");
         }
+        Console.ReadKey();
     }
 }
